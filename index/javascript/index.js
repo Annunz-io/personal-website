0f626f9ebf63cafb1521187dbcb053039d586bb3
Chris Annunziato <NeonMeteors@gmail.com>
2024-02-07 23:07:14 -0500

    fixed light and dark theme mode now properly working

--- index/javascript/index.js
+++ index/javascript/index.js
@@ -1,9 +1,18 @@
 init()
 
 function init() {
-    document.getElementById("icon-sun").style.display = "block";
-    document.getElementById("icon-moon").style.display = "none";
+    //init to light
+    //set a listener
+    document.getElementById("icon-sun").style.display = "none";
+    document.getElementById("icon-moon").style.display = "block";
     document.querySelector('.theme-container').addEventListener('click', switchTheme)
+
+    const rootElem = document.documentElement;
+    let dataTheme = rootElem.getAttribute('data-theme')
+    let oppositeComputedThemeColor = getComputedStyle(document.querySelector(":root[data-theme='" + dataTheme + "']"));
+    //we need to init the moon icon to the light theme and then switch the sun to dark theme upon click
+    let color = oppositeComputedThemeColor.getPropertyValue('--primary-text-color')
+    document.getElementById('icon-moon').style.fill =  color
 }
 
 function switchTheme() {
@@ -12,22 +21,27 @@ function switchTheme() {
     let newTheme = (dataTheme === 'light') ? 'dark' : 'light'
     rootElem.setAttribute('data-theme', newTheme)
     let computedStyle = getComputedStyle(document.querySelector(":root[data-theme='" + newTheme + "']"));
-    switchIcon(newTheme, computedStyle)
-}
-
-function switchIcon(newTheme, computedStyle) {
-    let sunIcon = document.getElementById('icon-sun')
-    let darkIcon = document.getElementById('icon-moon')
+    // let selectedElement = document.querySelector(`[data-theme="${newTheme}"]`);
+    // let computedStyle = getComputedStyle(selectedElement)
     let color = computedStyle.getPropertyValue('--primary-text-color')
 
+    //get computed style for dark icon
+
+    switchIcon(newTheme, color)
+}
+
+function switchIcon(newTheme, color) {
+    let sunIcon = document.getElementById('icon-sun')
+    let moonIcon = document.getElementById('icon-moon')
+
     if (newTheme === 'light') {
-        darkIcon.style.display = "none";
+        sunIcon.style.display = "none";
+        moonIcon.style.display = "block";
+        moonIcon.style.fill = color;
+    } else if (newTheme === 'dark') {
+        moonIcon.style.display = "none";
         sunIcon.style.display = "block";
         sunIcon.style.fill = color;
-    } else if (newTheme === 'dark') {
-        sunIcon.style.display = "none";
-        darkIcon.style.display = "block";
-        darkIcon.style.fill = color;
     }
 }
 
